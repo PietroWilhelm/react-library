f1d795e632a51ab13b67c5d7bcd06dc0f2dec454
PietroWilhelm <pietrowilhelm@git.com.br>
2025-08-14 16:24:57 -0300

    rotas e links para pagina criadas

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,27 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      
-      <Route path='/'>
-        <HomePage />
-      </Route>
 
-      <Route path='/search'>
-        <SearchBooksPage />
-      </Route>
-      
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+
+        <Route path='/search'>
+          <SearchBooksPage />
+        </Route>
+      </Switch>
 
       <Footer />
     </div>
