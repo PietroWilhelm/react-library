e6fe5b758bea7b084687ebeccf3042c49afd5d6f
PietroWilhelm <pietrowilhelm@git.com.br>
2025-08-14 17:50:08 -0300

    criação do componente BookCheckoutPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,32 @@ import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 
 export const App = () => {
   return (
-    <div>
+    <div className='d-flex flex-column min-vh-100'>
       <Navbar />
 
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/home' />
-        </Route>
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
 
-        <Route path='/home'>
-          <HomePage />
-        </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
 
-        <Route path='/search'>
-          <SearchBooksPage />
-        </Route>
-      </Switch>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage />
+          </Route>
+        </Switch>
+      </div> 
 
       <Footer />
     </div>
